3edf1c27a4cbbadab2f8320d53d821d3b07c3dd5
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-13 14:36:15 +0200

    Done for the day

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.logh("Connected");
+/**
+ * TODO: fixing the media screen  for navbar to be resposnive
+ *  when we click on the cart toggler icon the cart must show
+ *
+ */
+
+/**
+ * TODO: we must use localstorage or firebase to cart store items
+ */

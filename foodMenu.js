2f20d01595b7087a8713a51a33d0111b9b53313c
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-13 09:48:26 +0200

    Updates

--- foodMenu.js
+++ foodMenu.js
@@ -3,5 +3,11 @@ async function getMenu(path) {
   try {
     const resposne = await fetch(path);
     const data = await resposne.json();
-  } catch (error) {}
+    const menuItems = Object.entries(data);
+  } catch (error) {
+    console.log("Opps erro from Resource", error.message);
+  }
 }
+
+// call get menu and parse the url
+getMenu(base_path_url);

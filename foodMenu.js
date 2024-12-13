1afca7556f336f664f23996f7acd169c65f21fb8
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-13 09:55:12 +0200

    Added a funcyion to render the menu items

--- foodMenu.js
+++ foodMenu.js
@@ -1,9 +1,17 @@
 const base_path_url = "./data/foodMenu.json";
+const menuContainer = document.querySelector(".container-menu");
+/**
+ *
+ * @param {*} path
+ * @returns {Array} data
+ * This funcion gets menu items from a json file
+ */
 async function getMenu(path) {
   try {
     const resposne = await fetch(path);
     const data = await resposne.json();
-    const menuItems = Object.entries(data);
+    const menuItems = Object.values(data);
+    displayItems(menuItems);
   } catch (error) {
     console.log("Opps erro from Resource", error.message);
   }
@@ -11,3 +19,15 @@ async function getMenu(path) {
 
 // call get menu and parse the url
 getMenu(base_path_url);
+
+/**
+ *
+ * @param {*} data
+ * This is a render function , it loops on the list of items  and for each item
+ * it creates a card and display the data according to the specified properties
+ */
+function displayItems(data) {
+  data.forEach((item) => {
+    console.log(item);
+  });
+}

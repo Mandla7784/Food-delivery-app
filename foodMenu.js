627b6035134ca1badeefabbc660674bd9a08ea55
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-13 09:42:55 +0200

    Added an async function to get menu items from json file

--- foodMenu.js
+++ foodMenu.js
@@ -1,1 +1,7 @@
-console.log("Connected");
+const base_path_url = "./data/foodMenu.json";
+async function getMenu(path) {
+  try {
+    const resposne = await fetch(path);
+    const data = await resposne.json();
+  } catch (error) {}
+}

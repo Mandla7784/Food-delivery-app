1d694fa2c5f74a1d9805be35c4995234bca7a20a
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-17 09:08:45 +0200

    working on cart bugs

--- cartManger.js
+++ cartManger.js
@@ -1,6 +1,14 @@
 const cartItems = JSON.parse(localStorage.getItem("cartitems")) || [];
 const cartToggler = document.querySelector(".shoping-bag");
-
+const cartItemsContainer = document.querySelector(".cart-items");
 //events
+
+let items = JSON.stringify(cartItems).split(",");
+items.forEach((i) => {
+  if (i.includes("")) {
+    i.replace("button", " ");
+    console.log(i);
+  }
+});
 
 cartToggler.addEventListener("click", () => {});

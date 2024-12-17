ee289caf36be63ac93706ef3bf54e0a64c13782e
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-17 08:27:00 +0200

    Updates

--- cartManger.js
+++ cartManger.js
@@ -1,1 +1,6 @@
 const cartItems = JSON.parse(localStorage.getItem("cartitems")) || [];
+const cartToggler = document.querySelector(".shoping-bag");
+
+//events
+
+cartToggler.addEventListener("click", () => {});
